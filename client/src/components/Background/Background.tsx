7b23005007d2f0c4f60712d10b2e66e5625950ac
SkinnyDevi <xthesmartdartx@gmail.com>
2023-02-26 00:03:16 +0000

    Fixed css and ripple effect for safari

--- client/src/components/Background/Background.tsx
+++ client/src/components/Background/Background.tsx
@@ -1,29 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import WifiLogo from "../WifiLogo";
 import styles from "./Background.module.css";
 
 export default function Background() {
+  const [timer, setTimerComplete] = useState(false);
+
   useEffect(() => {
     setTimeout(() => {
-      document.querySelectorAll("[ripple-size]").forEach((r) => {
-        const ripple = r as HTMLDivElement;
-        const rippleSize = +r.getAttribute("ripple-size")!;
-
-        ripple.style.width = `${50 * rippleSize}px`;
-        ripple.style.height = `${50 * rippleSize}px`;
-      });
-    }, 1000);
+      setTimerComplete(true);
+    }, 100);
   }, []);
+
+  useEffect(() => {
+    document.querySelectorAll("[ripple-size]").forEach((r) => {
+      const ripple = r as HTMLDivElement;
+      const rippleSize = +r.getAttribute("ripple-size")!;
+
+      ripple.style.width = `${50 * rippleSize}px`;
+      ripple.style.height = `${50 * rippleSize}px`;
+    });
+  }, [timer]);
 
   return (
     <div className={styles.background}>
       <div className={styles.wifi}>
         <div className={styles.wifi_background}>
-          <div className={styles.ripple}>
-            <div ripple-size={1}></div>
-            <div ripple-size={2}></div>
-            <div ripple-size={3}></div>
-          </div>
+          {timer ? (
+            <div className={styles.ripple}>
+              <div ripple-size={1}></div>
+              <div ripple-size={2}></div>
+              <div ripple-size={3}></div>
+            </div>
+          ) : null}
           <div className={styles.signal_logo}>
             <WifiLogo />
           </div>
